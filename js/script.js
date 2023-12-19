44094cdd7013bfa9cf2902782dde3aff5680996b
KhrystynaAA <64550492+KhrystynaAA@users.noreply.github.com>
2023-12-19 22:23:33 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ const menu = [
         id: 2,
         title:"Монтана",
         Category: "pizza",
-        article: "",
+        article: "Піца",
         description: "Помідор, салямі, печериці, перець, кукурудза, сир",
         price: "Діаметр 22см - 80UAH <br> Діаметр 32см - 150UAH <br> Діаметр 50см - 260UAH",
         img: "img/products/pizza/montana.jpg"
@@ -60,7 +60,7 @@ const menu = [
         id: 3,
         title:"Тачіно",
         Category: "pizza",
-        article: "",
+        article: "Піца",
         description: "Курка, печериці, кукурудза, перець, цибуля, сир",
         price: "Діаметр 22см - 80UAH <br> Діаметр 32см - 150UAH <br> Діаметр 50см - 260UAH",
         img: "img/products/pizza/tachino.jpg"
@@ -69,13 +69,140 @@ const menu = [
         id: 4,
         title:"Капріча",
         Category: "pizza",
-        article: "",
+        article: "Піца",
         description: "Шинка, печериці, болгарський перець, сир",
         price: "Діаметр 22см - 80UAH <br> Діаметр 32см - 150UAH <br> Діаметр 50см - 260UAH",
         img: "img/products/pizza/capricha.jpg"
     },
     {
+        id: 5,
+        title:"Паза",
+        Category: "pizza",
+        article: "Піца",
+        description: "Курка, креветки, болгарський перець, сир",
+        price: "Діаметр 22см - 100UAH <br> Діаметр 32см - 200UAH <br> Діаметр 50см - 290UAH",
+        img: "img/products/pizza/paza.jpg"
+    },
+    {
+        id: 6,
+        title:"IV сири (з грушею)",
+        Category: "pizza",
+        article: "Піца",
+        description: "Моцарелла, ементаль, горгонзола, пармезан, спеції",
+        price: "Діаметр 22см - 100UAH <br> Діаметр 32см - 200UAH <br> Діаметр 50см - 290UAH",
+        img: "img/products/pizza/fourCheese.jpg"
+    },
+    {
+        id: 7,
+        title:"Полло",
+        Category: "pizza",
+        article: "Піца",
+        description: "Курочка, моцарелла, помідор, кукурудза",
+        price: "Діаметр 22см - 80UAH <br> Діаметр 32см - 150UAH <br> Діаметр 50см - 260UAH",
+        img: "img/products/pizza/polo.jpg"
+    },
+    {
+        id: 8,
+        title:"Мексиканська",
+        Category: "pizza",
+        article: "Піца",
+        description: "Курка, ананас, болгарський перець, сир",
+        price: "Діаметр 22см - 80UAH <br> Діаметр 32см - 150UAH <br> Діаметр 50см - 260UAH",
+        img: "img/products/pizza/mexykanska.jpg"
+    },
+    {
+        id: 9,
+        title:"Домашня",
+        Category: "pizza",
+        article: "Піца",
+        description: "Курка, печериці, помідор, цибуля, пекінська капуста, сир",
+        price: "Діаметр 22см - 80UAH <br> Діаметр 32см - 150UAH <br> Діаметр 50см - 260UAH",
+        img: "img/products/pizza/domashnia.jpg"
+    },
+    {
         id: 10,
+        title:"Карбонара",
+        Category: "pizza",
+        article: "Піца",
+        description: "Сир, бекон, печериці, яйце, пармезан",
+        price: "Діаметр 22см - 80UAH <br> Діаметр 32см - 150UAH <br> Діаметр 50см - 260UAH",
+        img: "img/products/pizza/carbonara.jpg"
+    },
+    {
+        id: 11,
+        title:"Салямі",
+        Category: "pizza",
+        article: "Піца",
+        description: "Салямі, сир",
+        price: "Діаметр 22см - 80UAH <br> Діаметр 32см - 140UAH <br> Діаметр 50см - 260UAH",
+        img: "img/products/pizza/saliami.jpg"
+    },
+    {
+        id: 12,
+        title:"Маджоре",
+        Category: "pizza",
+        article: "Піца",
+        description: "Філе лосося, перець, соус вершковий, сир",
+        price: "Діаметр 22см - 100UAH <br> Діаметр 32см - 200UAH <br> Діаметр 50см - 280UAH",
+        img: "img/products/pizza/majore.jpg"
+    },
+    {
+        id: 13,
+        title:"Тунець",
+        Category: "pizza",
+        article: "Піца",
+        description: "Тунець, цибуля, болгарський перець, кукурудза, сир",
+        price: "Діаметр 32см - 180UAH <br> Діаметр 50см - 270UAH",
+        img: "img/products/pizza/tune.jpg"
+    },
+    {
+        id: 14,
+        title:"Лігурія",
+        Category: "pizza",
+        article: "Піца",
+        description: "Шинка, печериці, болгарський перець, цибуля, сир + сирний соус",
+        price: "Діаметр 32см - 150UAH ",
+        img: "img/products/pizza/liguriia.jpg"
+    },
+    {
+        id: 15,
+        title:"Кальцоне (закрита піца)",
+        Category: "pizza",
+        article: "Піца",
+        description: "Курка, шинка, печериці, болгарський перець, сир + пармезан",
+        price: "Діаметр 22см - 80UAH <br> Діаметр 32см - 150UAH ",
+        img: "img/products/pizza/calcone.jpg"
+    },
+    {
+        id: 16,
+        title:"Морський коктейль",
+        Category: "pizza",
+        article: "Піца",
+        description: "Морепродукти, лимон, перець, сир",
+        price: "Діаметр 22см - 100UAH <br> Діаметр 32см - 180UAH <br> Діаметр 50см - 280UAH",
+        img: "img/products/pizza/seaCocktail.jpg"
+    },
+    {
+        id: 17,
+        title:"Прошутто",
+        Category: "pizza",
+        article: "Піца",
+        description: "Вершкова основа, печериці, оливки, прошутто, помідор, сир, пармезан",
+        price: "Діаметр 22см - 100UAH <br> Діаметр 32см - 200UAH <br> Діаметр 50см - 300UAH",
+        img: "img/products/pizza/proshuto.jpg"
+    },
+    {
+        id: 18,
+        title:"Подих дракона <strong style='color: red;'>(ГОСТРА)</strong>",
+        Category: "pizza",
+        article: "Піца",
+        description: "Чілі, салямі, оливки, помідор, сир",
+        price: "Діаметр 22см - 100UAH <br> Діаметр 32см - 150UAH <br> Діаметр 50см - 260UAH",
+        img: "img/products/pizza/podyhDragon.jpg"
+    },
+    
+    {
+        id: 50,
         title:"Каліфорнія",
         Category: "roly",
         article: "Роли",
@@ -90,24 +217,25 @@ window.addEventListener("DOMContentLoaded", ()=>{
 });
 
 function displayMenusItem(menuItem){
+    let name__article;
     let displayMenusItem = menuItem.map((item)=>{
-        return `
-        <h2 class="recommended__title">${item.article}</h2>
-
-        <div class="recommended__products" id="menu__products">
-          
-        <article class="product">
-          <img
-            src=${item.img}
-            class="product__photo"
-          >
-          <h3 class="title">${item.title}</h3>
-          <p class="Category">${item.description}</p>
-          <p class="price">${item.price}</p>
-        </article>
-        </div>`;
+        name__article=item.article;
+        return `      
+        
+			<div class="col-sm-12 col-lg-4 col-md-6 " >
+				<div class="card-menu">
+					<img src=${item.img} class="card-img-top">
+					<div class="card-body">
+					  <p class="card-text">
+                        <h3 class="title">${item.title}</h3>
+                        <p class="Category">${item.description}</p>
+                        <p class="price">${item.price}</p>
+					</p>
+					</div>
+				  </div>
+			</div>`;
     })
 
     displayMenusItem = displayMenusItem.join("");
-    sectionCenter.innerHTML = displayMenusItem;
+    sectionCenter.innerHTML = `<h2 class="recommended__title">${name__article}</h2>${displayMenusItem}`;
 }
